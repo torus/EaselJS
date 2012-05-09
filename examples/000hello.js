234161bb983f1ee294f488e0d7f674ead6e3957e
Toru Hisai <toru@torus.jp>
2012-05-10 00:22:03 +0900

    ikra

--- examples/000hello.js
+++ examples/000hello.js
@@ -24,31 +24,25 @@ function init() {
 
     var pos = []
 
-    for (var u = 3; u > 0; u --) {
-        for (var i = 0; i < 20; i ++) {
-            var x = canvas_w * Math.random() | 0
-            var y = canvas_h * Math.random() | 0
+    for (var i = 0; i < 50; i ++) {
+        var x = canvas_w * Math.random() | 0
+        var y = canvas_h * Math.random() | 0
+        pos.push([x, y])
+    }
 
-            var r = 30 * u
+    for (var r = 255; r > 0; r -= 16) {
+        for (var i in pos) {
+            var x = pos[i][0]
+            var y = pos[i][1]
 
-            if (ctx.getImageData(x, y, 1, 1).data[0] < 256 / u
-               ) {
-                // g.beginFill(Graphics.getRGB(63, 0, 0, 1)).drawCircle(x, y, 90)
-                // g.beginFill(Graphics.getRGB(127, 0, 0, 1)).drawCircle(x, y, 60)
-                // g.beginFill(Graphics.getRGB(255, 0, 0, 1)).drawCircle(x, y, 30)
+            var col = ctx.getImageData(x, y, 1, 1).data[0]
 
-                for (var v = 0; v < u; v ++) {
-                    g.beginFill(Graphics.getRGB(64 * (u - v) - 1, 0, 0, 1)).drawCircle(x, y, r * Math.pow(2, v - u))
-                    stage.addChild(shape)
-                }
-
-                // call update on the stage to make it render the current display list to the canvas:
-                stage.update();
-
-                pos.push([x, y, r])
-            }
+            g.beginFill(Graphics.getRGB(255 - r, 0, 0, 1)).drawCircle(x, y, r)
+            stage.addChild(shape)
         }
     }
+
+    stage.update();
 
     // g.clear()
 
