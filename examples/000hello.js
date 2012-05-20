6e27284273a1cc6076b19424c3410a6453a47ab6
Toru Hisai <toru@torus.jp>
2012-05-20 18:28:47 +0900

    refactoring

--- examples/000hello.js
+++ examples/000hello.js
@@ -24,38 +24,51 @@ function init() {
 
     var pos = []
 
+    var gen = function () {
+        var x = canvas_w * Math.random() | 0
+        var y = canvas_h * Math.random() | 0
+
+        var col = ctx.getImageData(x, y, 1, 1).data[0]
+        console.log(col)
+        var r = Math.min(255 - 16 - col, 63)
+
+        return {x: x, y: y, r: r}
+    }
+
     var f = function (k) {
-        if (k < 20) {
-            var x = canvas_w * Math.random() | 0
-            var y = canvas_h * Math.random() | 0
-            var r = 50 * Math.random() | 0
-            pos.push([x, y, r])
+        if (k < 100) {
+            var next = gen()
+            var x = next.x
+            var y = next.y
+            var r = next.r
 
-            for (var r = 255; r > 0; r -= 16) {
-                for (var i in pos) {
-                    var x = pos[i][0]
-                    var y = pos[i][1]
-                    var r_base = pos[i][2]
+            if (r > 0) {
+                pos.push([x, y, r])
 
-                    var col = ctx.getImageData(x, y, 1, 1).data[0]
+                for (var r = 255; r > 0; r -= 16) {
+                    for (var i in pos) {
+                        var x = pos[i][0]
+                        var y = pos[i][1]
+                        var r_base = pos[i][2]
 
-                    g.beginFill(Graphics.getRGB(255 - r, 0, 0, 1 / (r - 14))).
-                        drawCircle(x, y, r_base + r)
-                    stage.addChild(shape)
+                        g.beginFill(Graphics.getRGB(255 - r + 15, 0, 0, 1)).
+                            drawCircle(x, y, r_base + r)
+                        stage.addChild(shape)
+                    }
                 }
+                stage.update()
             }
-            stage.update()
 
             setTimeout(f, 1, k + 1)
+        } else {
+            for (var i in pos) {
+                g.beginStroke("white").beginFill("red").drawCircle(pos[i][0], pos[i][1], pos[i][2]).endStroke()
+            }
+            stage.update();
         }
     }
 
     f(0)
 
     // g.clear()
-
-    // for (var i in pos) {
-    //     g.beginStroke("white").beginFill("red").drawCircle(pos[i][0], pos[i][1], pos[i][2]).endStroke()
-    // }
-    // stage.update();
 }
